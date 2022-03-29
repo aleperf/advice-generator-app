edf2823dc149650081592fb88d3b513ddc1951cd
aleperf <aleperf@users.noreply.github.com>
2022-03-29 11:05:21 +0200

    change min-height blockquote in SentenceCard

--- src/components/SentenceCard.js
+++ src/components/SentenceCard.js
@@ -5,7 +5,7 @@ const SentenceCard = ({advice, onClick}) =>{
     return(
     <div className={classes.card}>
     <h2 className={classes['card__id']}>advice #{advice.id}</h2>
-    <blockquote>{advice.advice}</blockquote>
+    <blockquote>“{advice.advice}”</blockquote>
     <div className={classes['card__divider']}></div>
     <button className={classes['card__button']} onClick={onClick}>
     </button>
